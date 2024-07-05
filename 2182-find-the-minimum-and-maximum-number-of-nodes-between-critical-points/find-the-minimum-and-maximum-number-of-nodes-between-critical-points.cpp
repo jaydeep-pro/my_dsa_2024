b1fd336c37cf001aaf05790a36b923e88131db6b
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2024-07-05 14:56:13 +0530

    Time: 176 ms (28.76%) | Memory: 119.6 MB (35.95%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,54 +1,50 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+#include <vector>
+#include <climits>
+
+// // Definition for singly-linked list.
+// struct ListNode {
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+
 class Solution {
 public:
-    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        // jaydeepjangid
-        vector<int> v;
-        ListNode* temp = head->next;
+    std::vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        std::vector<int> criticalPoints;
+        ListNode* prevNode = head;
+        ListNode* currNode = head->next;
+        int index = 1;
 
-        int prev = head->val, curr = -1, next = -1;
-        int i = 1;
-
-        while (temp && temp->next) {
-            curr = temp->val;
-            next = temp->next->val;
-
-            if (((curr < prev) && (curr < next)) ||
-                ((curr > prev) && (curr > next))) {
-                v.push_back(i);
+        while (currNode && currNode->next) {
+            ListNode* nextNode = currNode->next;
+            if ((currNode->val < prevNode->val && currNode->val < nextNode->val) ||
+                (currNode->val > prevNode->val && currNode->val > nextNode->val)) {
+                criticalPoints.push_back(index);
             }
-
-            i++;
-
-            prev = curr;
-            temp = temp->next;
+            prevNode = currNode;
+            currNode = nextNode;
+            index++;
         }
 
-        vector<int> ans;
-        if (v.size() < 2) {
-            ans.push_back(-1);
-            ans.push_back(-1);
-            return ans;
-        }
-        int mini = INT_MAX;
-        int maxi = INT_MIN;
-
-        for (int i = 1; i < v.size(); i++) {
-            mini = min(mini, (v[i] - v[i - 1]));
+        std::vector<int> result;
+        if (criticalPoints.size() < 2) {
+            result.push_back(-1);
+            result.push_back(-1);
+            return result;
         }
 
-        ans.push_back(mini);
-        ans.push_back(v[v.size() - 1] - v[0]);
+        int minDist = INT_MAX;
+        for (int i = 1; i < criticalPoints.size(); i++) {
+            minDist = std::min(minDist, criticalPoints[i] - criticalPoints[i - 1]);
+        }
+        int maxDist = criticalPoints.back() - criticalPoints.front();
 
-        return ans;
+        result.push_back(minDist);
+        result.push_back(maxDist);
+
+        return result;
     }
 };
