6cd2349f714647ad14e8889f7083f8abb689b135
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2024-09-17 12:08:33 +0530

    Time: 5 ms (15.91%) | Memory: 9.3 MB (16.29%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
+
+vector<string> splitBySpace(const string& str) {
+    vector<string> result;
+    string word = "";
+    
+    for (char c : str) {
+        if (c == ' ') {
+            if (!word.empty()) {
+                result.push_back(word);
+                word = "";
+            }
+        } else {
+            word += c;
+        }
+    }
+    
+    if (!word.empty()) {
+        result.push_back(word);
+    }
+    
+    return result;
+}
     vector<string> uncommonFromSentences(string s1, string s2) {
         unordered_map<string, int> mp;
-        string word;
-        stringstream ss1(s1), ss2(s2);
 
-        while (ss1 >> word) {
-            mp[word]++;
+       vector<string> splittedS1 = splitBySpace(s1);
+      vector<string> splittedS2 = splitBySpace(s2);
+
+
+        for (int i=0;i<splittedS1.size();i++) {
+            mp[splittedS1[i]]++;
         }
-
-        while (ss2 >> word) {
-            mp[word]++;
+        for (int i=0;i<splittedS2.size();i++) {
+            mp[splittedS2[i]]++;
         }
-
         vector<string> ans;
 
         for (auto it : mp) {
