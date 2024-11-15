b9224447b014773d9901043975eb267155db1c7e
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2024-11-15 14:44:50 +0530

    Time: 0 ms (100.00%) | Memory: 69.5 MB (85.81%) - LeetSync

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int findLengthOfShortestSubarray(vector<int>& arr) {
-        int left =0, right = arr.size() -1 , n = arr.size();
+        int left = 0, right = arr.size() - 1, n = arr.size();
 
-        while(left + 1 < n && arr[left] <= arr[left + 1]) left++;
-        while(right - 1 >=0 && arr[right] >= arr[right - 1]) right--;
+        while (left + 1 < n && arr[left] <= arr[left + 1])
+            left++;
+        while (right - 1 >= 0 && arr[right] >= arr[right - 1])
+            right--;
 
+        if (left >= right)
+            return 0;
 
-        if(left >= right) return 0;
+        int ans = right, s = 0, e = right;
 
-        int ans =right, s=0, e = right;
-
-        while(s <= left){
-while(e < n && arr[e] < arr[s]) e++;
-ans = min(ans, e-s-1);
-s++;
+        while (s <= left) {
+            while (e < n && arr[e] < arr[s])
+                e++;
+            ans = min(ans, e - s - 1);
+            s++;
         }
         return ans;
     }
