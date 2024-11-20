bc2902bbd1b687e4d20182475d74f3f8076bae49
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2024-11-20 20:48:49 +0530

    Time: 114 ms (52.83%) | Memory: 95.2 MB (56.67%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,49 +1,24 @@
 class Solution {
 public:
+#define ll long long
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        // all the elements should be different
-
-        unordered_map<int, int> mp;
-        long long temp = 0;
-        int len = 0;
-
-        if (k > nums.size())
-            return 0;
-
-        for (int i = 0; i < k; i++) {
-             temp += nums[i];
-            if (mp[nums[i]] == 0) {
-                len++;
-            }
-            mp[nums[i]]++;
-        }
-
-        long long ans = 0;
-
-        if (len == k){
-            ans = temp;
-}
-
-        for (int i = k; i < nums.size(); i++) {
-      // Remove the element going out of the window
-            mp[nums[i - k]]--;
-            if (mp[nums[i - k]] == 0) {
-                len--;
-            }
-
-           // Add the new element coming into the window
-            temp += nums[i] - nums[i - k];
-            if (mp[nums[i]] == 0) {
-                len++;
-            }
-            mp[nums[i]]++;
-
-            // Check if the current window has all unique elements
-            if (len == k) {
-                ans = max(ans, temp);
+        long ans = 0, sum = 0;
+        unordered_map<int, int>mp;
+        int i = 0, j = 0, n = nums.size();
+        while(j < n)
+        {
+            sum += nums[j];
+            mp[nums[j++]]++;
+            if(j - i == k)
+            {
+                if(mp.size() == k)
+                    ans = max(ans, sum);
+                mp[nums[i]]--;
+                sum -= nums[i];
+                if(not mp[nums[i]]) mp.erase(nums[i]);
+                i++;
             }
         }
-
         return ans;
     }
 };
