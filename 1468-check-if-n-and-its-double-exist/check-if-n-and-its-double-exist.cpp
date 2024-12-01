584cd2708f055a98c616aaaaf7dff73b8f4b613d
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2024-12-01 16:59:51 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (24.36%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+       sort(arr.begin(), arr.end());
+
+       for(int i=0;i<arr.size()-1;i++){
+         for (int j = i + 1; j < arr.size(); j++) {
+                if (arr[i] * 2 == arr[j] || arr[i] == 2 * arr[j]) {
+                    return true;
+                }
+            }
+       }
+
+       return false;
+    }
+};
