377d4f252d117faa4adf58e7446087541bd1381e
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2025-02-03 11:17:30 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (23.00%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-       int i =0;
-       for( i=0; i< nums.size() - 1 ;i++){
-        if(nums[i] > nums[i+1]){
-            break;
+        int i = 0;
+        for (i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                break;
+            }
         }
-       }
 
-if( i == nums.size() - 1 ) return true;
-if( (i != 0) &&(nums[0] < nums[nums.size() - 1])) return false;
+        if (i == nums.size() - 1)
+            return true;
+        if ((i != 0) && (nums[0] < nums[nums.size() - 1]))
+            return false;
 
+        for (int j = i + 1; j < nums.size() - 1; j++) {
+            if ((nums[j] > nums[j + 1]) || (nums[0] < nums[nums.size() - 1]))
+                return false;
+        }
 
-       for(int j =i+1;j<nums.size()-1;j++){
-        if((nums[j] > nums[j+1]) || (nums[0] < nums[nums.size() - 1])) return false;
-       }
-
-       
-
-       return true;
+        return true;
     }
 };
