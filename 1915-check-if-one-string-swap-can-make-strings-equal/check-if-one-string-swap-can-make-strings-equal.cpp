725f1d4dbbfb43aa14da69501eb6796e6a5f5b79
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2025-02-05 09:36:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (97.48%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
@@ -6,26 +6,35 @@ public:
 
         if (s1.length() != s2.length())
             return false;
-        string temp = "";
+       
+   char first = '\0', second = '\0', third = '\0', four = '\0';
         int cnt = 0;
         for (int i = 0; i < s1.size(); i++) {
             if (s1[i] != s2[i]) {
                 cnt++;
-                if(cnt > 2){
+                if (cnt > 2) {
                     return false;
                 }
-                temp = temp + s1[i] + s2[i];
+               if(first == '\0' ){
+                first = s1[i];
+                second = s2[i];
+               }
+               else{
+                third = s1[i];
+                four = s2[i];
+               }
             }
         }
 
+        if (cnt == 1)
+            return false;
 
-        if(cnt == 1) return false;
-
-        if(cnt ==2 ){
-             string tempR = temp;
-            reverse(temp.begin(), temp.end());
-            if(temp == tempR) return true;
-            else return false;
+        if (cnt == 2) {
+            
+            if (first == four && third == second)
+                return true;
+            else
+                return false;
         }
 
         return true;
